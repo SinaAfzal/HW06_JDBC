652a4ad1359dbeadb6ab8fdf7fb941e3a706afea
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-13 19:34:11 +0330

    develope class JDBCConnection

--- src/main/java/connection/JDBCConnection.java
+++ src/main/java/connection/JDBCConnection.java
@@ -1,4 +1,16 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCConnection {
+    private final Connection connection= DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","12369874");
+
+    public JDBCConnection() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
