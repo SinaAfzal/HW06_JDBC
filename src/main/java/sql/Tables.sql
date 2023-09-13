7c8ac932ab33cf742e9d7ab947d8e859a3cc3629
Sina Afzalsoltani <s.afzalsoltani@gmail.com>
2023-09-13 19:26:16 +0330

    Create Tables.sql and add three tables to it

---
+++ src/main/java/sql/Tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS authors
+(
+    id        serial PRIMARY KEY,
+    firstName varchar(50) NOT NULL,
+    lastName  varchar(50) NOT NULL,
+    age       int
+);
+CREATE TABLE IF NOT EXISTS books
+(
+    id      serial PRIMARY KEY,
+    title   varchar(50) NOT NULL,
+    pubYear int         NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS authorship
+(
+    author_id int,
+    book_id   int,
+    PRIMARY KEY (author_id, book_id),
+    FOREIGN KEY (author_id) REFERENCES Authors (id),
+    FOREIGN KEY (book_id) REFERENCES Books (id)
+);
